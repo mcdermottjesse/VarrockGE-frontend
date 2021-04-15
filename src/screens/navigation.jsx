b3709236401504be23455f8f529c5797efd5af70
Ray Yiu <rayyiu66@gmail.com>
2021-04-15 01:03:52 -0700

    added home page

--- src/screens/navigation.jsx
+++ src/screens/navigation.jsx
@@ -4,7 +4,7 @@ export default function Navigation() {
 
   return (
   <nav>
-  <Link to="/">Home </Link>
+  {/* <Link to="/">Home </Link> */}
   <Link to="/admin">Admin </Link>
   <Link to="/login">Login </Link>
   <Link to="/widgets">Widgets </Link>
